1433ca85a9c7a4a4404390768be8a531b7c0ba1f
LOSAT Giovanni <giolstpro@gmail.com>
2025-03-20 13:59:45 +0100

    rectification models/index.js

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.js')[env];
 
 let sequelize;
 if (config.use_env_variable) {
