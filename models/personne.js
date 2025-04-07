3bbe814df51d1d65045b1db1bb9a478f285f6a05
Corentin Deldalle <151193169+CorentinDeldalle19@users.noreply.github.com>
2025-04-07 16:01:21 +0200

    Update database

--- models/personne.js
+++ models/personne.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    image: {
+    profile: {
       type: DataTypes.STRING,
       allowNull: false
     }
